c10674036cd559c33537af8825e841fc1ee0c170
AsangiSathsarani <38518830+AsangiSathsarani@users.noreply.github.com>
2019-03-18 13:53:24 +0530

    MERN-Stack Some change files

---
+++ MERN-App/backend/frontend/src/components/Landing.js
@@ -0,0 +1,21 @@
+import React ,{Component} from 'react'
+
+class Landing extends Component{
+    render(){
+        return(
+            <div className ="container">
+                <div className ="jumbotrom mt-5">
+                    <div className ="col-sm-8 mx-auto">
+                            <h1 className ="text-container">WELCOME</h1>
+                    </div>
+                </div>
+            </div>
+        )
+           
+        
+    }
+
+
+}
+
+export default Landing
